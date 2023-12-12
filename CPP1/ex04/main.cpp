44e9e27f1e035e3c02aaf1194172a77354213451
sathyagimenes <sathya.gimenes@gmail.com>
2023-12-11 21:22:08 -0300

    CPP01 ex04

--- CPP1/ex04/main.cpp
+++ CPP1/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 22:36:12 by sde-cama          #+#    #+#             */
-/*   Updated: 2023/12/09 19:06:12 by sde-cama         ###   ########.fr       */
+/*   Updated: 2023/12/11 20:54:06 by sde-cama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 int main(int argc, char *argv[])
 {
-    if (check_argc(argc))
+    if (!check_argc(argc))
         return(1);
 
     std::ifstream	infile(argv[1]);
-    if (check_in_file(infile))
+    if (!check_in_file(infile))
         return(1);
 
     std::string fileName(argv[1]);
     fileName += ".repalce";
     std::ofstream   outfile(fileName.c_str());
-    if (check_out_file(outfile))
+    if (!check_out_file(outfile))
         return (1);
 
     std::string s1(argv[2]);
--- CPP1/ex04/utils.cpp
+++ CPP1/ex04/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 22:30:16 by sde-cama          #+#    #+#             */
-/*   Updated: 2023/12/09 18:40:53 by sde-cama         ###   ########.fr       */
+/*   Updated: 2023/12/11 20:59:07 by sde-cama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ bool	check_in_file(std::ifstream &in)
 {
 	if (!in.is_open() || in.fail())
 	{
-		std::cerr << "ERROR: could not open file." << std::endl;
+		std::cerr << "ERROR: could not open infile." << std::endl;
 		return (false);
 	}
 	return (true);
@@ -37,8 +37,31 @@ bool	check_out_file(std::ofstream &out)
 {
 	if (!out.is_open() || out.fail())
 	{
-		std::cerr << "ERROR: could not create file." << std::endl;
+		std::cerr << "ERROR: could not create outfile." << std::endl;
 		return (false);
 	}
 	return (true);
+}
+
+void	replace(std::ifstream &in, std::ofstream &out, std::string s1, std::string s2)
+{
+	std::string line;
+	size_t s1_pos;
+	long unsigned int index;
+
+	while(in.good())
+	{
+		std::getline(in, line);
+		index = 0;
+		while (true)
+		{
+			s1_pos = line.find(s1, index);//acha a posição da s1
+			if (s1_pos == std::string::npos)//se não acha encerra o loop
+				break;
+			out << line.substr(index, s1_pos - index);//copia ate a incidência da s1
+			out << s2;//copia a s2 no lugar da s1;
+			index = (s1_pos + s1.length());//pula a s1
+		}
+		out << line.substr(index) << std::endl;
+	}
 }
