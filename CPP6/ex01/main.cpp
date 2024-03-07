2a22d530525a26a7386c96384f661cf794c7fbbb
sathyagimenes <sathya.gimenes@gmail.com>
2024-03-06 21:02:56 -0300

    fix: CPP6/ex01 not instatialized

--- CPP6/ex01/main.cpp
+++ CPP6/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 09:51:08 by sde-cama          #+#    #+#             */
-/*   Updated: 2024/03/06 20:53:31 by sde-cama         ###   ########.fr       */
+/*   Updated: 2024/03/06 21:02:26 by sde-cama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 int main( void )
 {
-	std::cout << "=== Constructor List ===" << std::endl;
 	Data		*dataOriginal = new Data();
-	Serializer s = Serializer();
 	uintptr_t	dataSerialized;
 	Data		*dataDeserialized;
 	dataOriginal->name = "Sathya";
 	dataOriginal->age = 31;
 
 	std::cout << "\n> Original Data:\t\t" << dataOriginal->name << std::endl;
-	dataSerialized = s.serialize(dataOriginal);
+	dataSerialized = Serializer::serialize(dataOriginal);
 	std::cout << "> Serialized Data:\t" << dataSerialized << std::endl;
-	dataDeserialized = s.deserialize(dataSerialized);
+	dataDeserialized = Serializer::deserialize(dataSerialized);
 	
 	if (dataOriginal == dataDeserialized)
 	{
@@ -35,8 +33,7 @@ int main( void )
 	{
 		std::cerr << "> Data not deserialized!" << std::endl;
 	}
-	
-	std::cout << "\n=== Destructor List ===" << std::endl;
+
 	delete dataOriginal;
 	return (0);
 }
